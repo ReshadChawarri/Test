ab44f7fda1bfbd7a91d0694bb7fd1223fbf9c9cf
Reshad Chawarri <reshad.chawarri@gmail.com>
2023-12-10 17:42:07 +0100

    Test

---
+++ src/lib/colors.js
@@ -0,0 +1,7 @@
+export default {
+    primary: "#EE6C4D",
+    secondary: "#A9E4EF",
+    mint: "#3AB795",
+    moss: "#8B9556",
+    red: "#EE6C4D",
+}
---
+++ src/lib/components/Test.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Card } from "flowbite-svelte";
+
+ 
+    export let message = "Hello World"
+    
+    function addExclamation(){
+        message += "!"
+    }
+    </script>
+    
+    <h1 on:click={addExclamation}>  {message}  </h1>
+    <Card>  
+     <slot>
+        test
+     </slot>   
+    </Card>
